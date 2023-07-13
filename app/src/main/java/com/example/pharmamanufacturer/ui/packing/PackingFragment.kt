03960a8547133aa6b15c9a63952af91ed0587d25
Elkrrai <elkrrai@lamsaworld.com>
2023-07-13 16:34:03 +0300

    use compose in fragment

--- app/src/main/java/com/example/pharmamanufacturer/ui/packing/PackingFragment.kt
+++ app/src/main/java/com/example/pharmamanufacturer/ui/packing/PackingFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
@@ -17,20 +16,15 @@ class PackingFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = LinearLayout(requireContext()).apply {
-        addView(
-            ComposeView(requireContext()).apply {
-                setViewCompositionStrategy(
-                    ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
-                )
-
-/*                setContent {
-                    MaterialTheme {
-                        Text("I AM BATMAN")
-                    }
-                }*/
-
-            }
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(
+            ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
         )
+
+        setContent {
+            MaterialTheme {
+                Text("I AM BATMAN")
+            }
+        }
     }
 }
