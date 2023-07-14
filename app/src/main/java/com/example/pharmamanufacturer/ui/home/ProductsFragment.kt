8364626ac420b55ed81d2db0620df5c2b3b0a578
Elkrrai <elkrrai@lamsaworld.com>
2023-07-14 14:29:24 +0300

    add Theme.kt

--- app/src/main/java/com/example/pharmamanufacturer/ui/home/ProductsFragment.kt
+++ app/src/main/java/com/example/pharmamanufacturer/ui/home/ProductsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.pharmamanufacturer.databinding.FragmentHomeBinding
+import com.example.pharmamanufacturer.databinding.FragmentProductsBinding
 
-class HomeFragment : Fragment() {
+class ProductsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentProductsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,7 +22,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
