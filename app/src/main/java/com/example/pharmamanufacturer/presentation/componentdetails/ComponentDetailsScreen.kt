3a4dc53cb6e20b3730e4733874c332fc978b5cd3
Elkrrai <elkrrai@lamsaworld.com>
2023-08-06 00:24:20 +0300

    style text

--- app/src/main/java/com/example/pharmamanufacturer/presentation/componentdetails/ComponentDetailsScreen.kt
+++ app/src/main/java/com/example/pharmamanufacturer/presentation/componentdetails/ComponentDetailsScreen.kt
@@ -6,7 +6,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pharmamanufacturer.presentation.theme.DarkRed
+import com.example.pharmamanufacturer.presentation.theme.DeepBlue
 
 @Composable
 fun ComponentDetailsScreen() {
@@ -17,8 +25,32 @@ fun ComponentDetailsScreen() {
     ) {
         vm.selectedComponent?.let { component ->
             Text(
-                text = "Name = ${component.name} \n" +
-                    "Amount = ${component.amount}"
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = DeepBlue,
+                            fontFamily = FontFamily.SansSerif,
+                            fontWeight = FontWeight.Bold
+                        )
+                    ) {
+                        append("Name")
+                    }
+
+                    append(" = ${component.name}\n")
+
+                    withStyle(
+                        style = SpanStyle(
+                            color = DarkRed,
+                            fontFamily = FontFamily.Serif,
+                            fontWeight = FontWeight.Medium
+                        )
+                    ) {
+                        append("Amount")
+                    }
+
+                    append(" = ${component.amount}")
+                },
+                fontStyle = FontStyle.Italic
             )
         }
     }
